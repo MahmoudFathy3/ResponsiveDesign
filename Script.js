d85dcc15f29931c726fb940467cc99027e802223
MahmoudFathyCode <88001901+MahmoudFathyCode@users.noreply.github.com>
2021-07-28 21:07:47 +0200

    More Features

---
+++ Script.js
@@ -0,0 +1,26 @@
+ const icon = document.getElementsByClassName('icon')[0]
+const nav = documnet.getElementsByClassName('nav')[0]
+
+/*
+icon.addEventListener('click', () =>{
+     nav.classList.icon('active')
+});  
+
+
+
+document.getElementById("button").onclick = function(){
+    document.getElementById("p").innerHTML ="Welcome Mahmoud in JavaScript";
+    document.getElementById("button").style.display= "none";
+
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
+
+  */
